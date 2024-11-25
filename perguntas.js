2adbffafe5d62a0657787c52b80c60772bba0936
Juan <00001207625498sp@al.educacao.sp.gov.br>
2024-11-25 13:39:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Equipamentos',
+    'Qual é o nome correto de se chamar aquele equipamento no avião que faz ele ter potência?',
+    'Você pensou na turbina ou elice né? Pois é, esse é um erro comum. E chamando de motor simplesmente.'
+)
+
+criaCartao(
+    'Modelos',
+    'Qual é o nome do avião que é famoso pelo seu Joystick?',
+    'A Airbus é famosa pelos seus aviões com O Joystick'
+)
+
+criaCartao(
+    'Pilotos',
+    'Qual é o nome do piloto que fez a manobra no Rio Hudson? ',
+    'O famoso é reconhecido piloto Chesley Sullenberger'
+)
+
+criaCartao(
+    'Modelos',
+    'Quais são os dois modelos de avião mais vendidos no mundo?',
+    ' Airbus A320 e o Boing 737'
+)
